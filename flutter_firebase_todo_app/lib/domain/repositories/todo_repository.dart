24897c6b40b8cb8c40b6122eabd1c221bf02ed9f
finn-ev <finnevermann@gmx.de>
2022-07-07 19:28:10 +0200

    feature/todo-firebase-classes

---
+++ flutter_firebase_todo_app/lib/domain/repositories/todo_repository.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_firebase_todo_app/core/failures/todo_failures.dart';
+import 'package:flutter_firebase_todo_app/domain/entities/id.dart';
+import 'package:flutter_firebase_todo_app/domain/entities/todo.dart';
+
+abstract class TodoRepository {
+  Stream<Either<TodoFailure, List<Todo>>> watchAll();
+
+  Future<Either<TodoFailure, Unit>> create(Todo todo);
+
+  Future<Either<TodoFailure, Unit>> update(Todo todo);
+
+  Future<Either<TodoFailure, Unit>> delete(UniqueID id);
+}
